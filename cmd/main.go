3c719f30af3393aafdfb61a6395be98853a98f8b
dong.havan <dong.havan@vib.com.vn>
2022-09-20 11:03:51 +0700

    update config

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,17 @@
 package cmd
+
+import "github.com/gin-gonic/gin"
+import (
+	log
+	"github.com/gin-gonic/gin"
+	"github.com/vandong9/go-grpc-api-gateway/pkg/config"
+)
+
+func main() {
+	c, err := config.LoadConfig();
+	if err != nil {
+		log.Fatalln("Failed at config", err);
+	}
+
+	r := gin.Default();
+}
