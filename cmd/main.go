c54f244b53f038cb4d957ed3e3068c87f0c22f2d
dong.havan <dong.havan@vib.com.vn>
2022-09-20 13:13:26 +0700

    update protoc

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,17 @@
 package cmd
 
-import "github.com/gin-gonic/gin"
 import (
-	log
 	"github.com/gin-gonic/gin"
-	"github.com/vandong9/go-grpc-api-gateway/pkg/config"
+	"github.com/vandong9/go-grpc-api-gateway.git/pkg/config"
+	"log"
 )
 
 func main() {
-	c, err := config.LoadConfig();
+	c, err := config.LoadConfig()
 	if err != nil {
-		log.Fatalln("Failed at config", err);
+		log.Fatalln("Failed at config", err)
 	}
 
-	r := gin.Default();
+	r := gin.Default()
+
 }
