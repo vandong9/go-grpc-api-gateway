0abbec36664667db993ae97f5d36ded72ce8a5ce
dong.havan <dong.havan@vib.com.vn>
2022-09-20 11:20:15 +0700

    update config

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,5 +1,27 @@
 package config
 
-func LoadConfig() {
+import "github.com/spf13/viper"
 
+type Config struct {
+	Port          string `mapstructure:"PORT"`
+	AuthSvcUrl    string `mapstructure:"AUTH_SVC_URL"`
+	ProductSvcUrl string `mapstructure:"PRODUCT_SVC_URL"`
+	OrderSvcUrl   string `mapstructure:"ORDER_SVC_URL"`
+}
+
+func LoadConfig() (c Config, err error) {
+	viper.AddConfigPath("./pkg/config/envs")
+	viper.SetConfigName("dev")
+	viper.SetConfigType("evn")
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+
+	if err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&c)
+	return
 }
